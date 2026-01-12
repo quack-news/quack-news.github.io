ebca5720fb26967f956b19b397ecf2bc973f4dc8
imgabe <gabrielpmonte@hotmail.com>
2026-01-12 11:53:50 -0300

    chore: configurar deploy para GitHub Pages via GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/',
+  site: 'https://quack-news.github.io',
+  base: '/quack-news',
   trailingSlash: "always",
   integrations: [svelte()]
 });
