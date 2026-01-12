506926cad221d710ad99e44e075b867c78bc18ac
imgabe <gabrielpmonte@hotmail.com>
2026-01-12 17:54:42 -0300

    feat: implement FeaturedCard and improve SectionHeader design

--- src/components/blog/index.js
+++ src/components/blog/index.js
@@ -1,2 +1,3 @@
 export { default as ContentCard } from './ContentCard.astro';
+export { default as FeaturedCard } from './FeaturedCard.astro';
 export { default as SectionHeader } from './SectionHeader.astro';
